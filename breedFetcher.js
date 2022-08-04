d472aa5ccf7b8e1a08f37a41cd257f0e09d37336
Shane <navysupra@gmail.com>
2022-08-04 22:00:30 +0000

    Appears to cover the required edge-cases, functioning.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,14 @@
 /* Required */
 const request = require(`request`);
+
+/* Arguments */
 const args = process.argv.slice(2);
+const SearchKey = args[0];
 
 /* Tcp:Http */
 const Url = `https://api.thecatapi.com/v1/breeds/search`;
-const SearchKey = args[0];
 
 /* Functions */
-const deserializeJson = (json) => {
-  return JSON.parse(json)[0];
-};
 const exitApp = (exitMessage) => {
   console.log(exitMessage);
   process.exit();
@@ -17,7 +16,7 @@ const exitApp = (exitMessage) => {
 
 /* Execution */
 // Check and make sure there is only one argument
-if (args.length !== 1) exitApp(`No search key provided. exiting.`);
+if (args.length !== 1) exitApp(`Incorrect argument format.\n  $> node breedFetcher.js siamese`);
 // Query the api with the presented search key
 request(`${Url}?q=${SearchKey}`, (error, response, body) => {
   if (error) exitApp(error);
