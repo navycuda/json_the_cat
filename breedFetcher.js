d33d28f4ff5228e01996e67304c604326c3c7a4f
Shane <navysupra@gmail.com>
2022-08-04 21:53:51 +0000

    getting closer on implementation, few small bugs

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,27 @@
 /* Required */
 const request = require(`request`);
 const args = process.argv.slice(2);
-if (args.length === 0) process.exit();
 
 /* Tcp:Http */
 const Url = `https://api.thecatapi.com/v1/breeds/search`;
-const SearchKey = `?q=${args[0]}`;
+const SearchKey = args[0];
 
 /* Functions */
 const deserializeJson = (json) => {
   return JSON.parse(json)[0];
 };
 const exitApp = (exitMessage) => {
+  console.log(exitMessage);
   process.exit();
 };
 
 /* Execution */
-request(Url + SearchKey, (error, response, body) => {
+// Check and make sure there is only one argument
+if (args.length !== 1) exitApp(`No search key provided. exiting.`);
+// Query the api with the presented search key
+request(`${Url}?q=${SearchKey}`, (error, response, body) => {
   if (error) exitApp(error);
-  const kitty = deserializeJson(body);
-  console.log(kitty);
+  const kitty = JSON.parse(body)[0];
+  if (!kitty) exitApp(`Breed '${SearchKey}' not found.`);
+  console.log(kitty.description);
 });
