8ad08cf8a19f6a03d9fddf3597a63559e34d8dfa
Shane <navysupra@gmail.com>
2022-08-05 00:34:09 +0000

    committing progres... no testing yet

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const args = process.argv.slice(2);
 
 /* Export Functions */
 /* Local Functions */
-const outputToConsole = (error, description) {
-
+const outputToConsole = (error, description) => {
+  if (error) console.log(error);
+  else console.log(description);
 };
+
 /* Execution & Test Data */
-if (args.length !== 1) fetchBreedDescription ()
+// if argument lengths are not correct, send back this error notice
+// otherwise move onto the next phase
+if (args.length !== 1) fetchBreedDescription(`ARGS!`, outputToConsole);
+else fetchBreedDescription(args[0], outputToConsole);
 
 /* Exports */
