a6190342637a735bd312f54f7958cd26455c3317
Shane <navysupra@gmail.com>
2022-08-05 00:13:14 +0000

    committing mid refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,25 @@
 /* Require */
+const request = require(`request`);
+
 /* Tcp:Http */
+const ApiUrl = `https://api.thecatapi.com/v1/breeds/search?q=`;
+
+
 /* Arguments */
 /* Export Functions */
 const fetchBreedDescription = (breed, callback) => {
+  request(ApiUrl, (error, response, body) => {
 
 
 
-  callback(`error`, `description`);
+
+
+
+
+
+    
+    callback(`error`, `description`);
+  });
 };
 
 /* Local Functions */
